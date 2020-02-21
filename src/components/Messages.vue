2745f8016a4a999e61857c1f1be24f775f971ec0
Lucas Gomes <lucasgomesp22@gmail.com>
2020-02-21 15:30:13 -0300

    installing vuetify

---
+++ src/components/Messages.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h4 class="display-1">Messages</h4>
+
+    <v-list-item  v-for="(message, index) in messages" :key="index" @click="true">
+      <v-list-item-content>
+        <v-list-item-title>{{ message }}</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "messages",
+  data() {
+    return {
+      messages: ["hello", "hi"]
+    };
+  }
+};
+</script>
