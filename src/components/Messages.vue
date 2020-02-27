8a3f55ddf4db3f33c7e1b9ec6e704bf8a45e7f2b
Lucas Gomes <lucasgomesp22@gmail.com>
2020-02-27 12:22:12 -0300

    add axios and request messages api

--- src/components/Messages.vue
+++ src/components/Messages.vue
@@ -2,7 +2,7 @@
   <div>
     <h4 class="display-1">Messages</h4>
 
-    <v-list-item  v-for="(message, index) in messages" :key="index" @click="true">
+    <v-list-item v-for="(message, index) in messages" :key="index" @click="true">
       <v-list-item-content>
         <v-list-item-title>{{ message }}</v-list-item-title>
       </v-list-item-content>
@@ -11,12 +11,17 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "messages",
   data() {
     return {
-      messages: ["hello", "hi"]
+      // messages: ["hello", "hi"]
     };
+  },
+  async created() {
+    this.messages = (await axios.get("http://localhost:3000/messages")).data;
   }
 };
 </script>
